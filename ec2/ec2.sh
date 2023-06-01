eff9ecc99cb122d6c8c0051bb98c36d4e5b8b082
vanshaj <vanshajdhar@gmail.com>
2023-06-01 04:11:21 +0000

    added code for NAT gateway to give access to internet inside EC2 within pvt subnet

--- ec2/ec2.sh
+++ ec2/ec2.sh
@@ -2,10 +2,10 @@
 default_vpc_id=`aws ec2 describe-vpcs | jq '.Vpcs[].VpcId'`
 echo "Default Vpc Id is $default_vpc_id"
 
-vpc_id = `aws ec2 create-vpc \
+vpc_id=`aws ec2 create-vpc \
 	--cidr-block 172.32.0.0/16 \
 	--tag-specification '[{"ResourceType": "vpc", "Tags": [{"Key": "Name", "Value": "FirstVPC"}]}]' \
-	--instance-tenancy default | jq '.Vpc.VpcId'`
+	--instance-tenancy default | jq '.Vpc.VpcId' | tr -d '"'`
 echo "newly created vpc id is $vpc_id"
 
 # Get the Subets Ids 
@@ -16,12 +16,17 @@ echo $all_subnet_cidrs
 subnet_id=`aws ec2 create-subnet \
 	--vpc-id $vpc_id \
 	--availability-zone us-east-1a \
-	--cidr-block 172.32.1.0/24 | jq '.Subnet.SubnetId'`
-echo "Created new subnet with id $subnet_id"
+	--cidr-block 172.32.1.0/24 | jq '.Subnet.SubnetId' | tr -d '"'`
+echo "Created new public subnet with id $subnet_id"
+subnet_id_pvt=`aws ec2 create-subnet \
+		--vpc-id $vpc_id \
+		--availability-zone us-east-1a \
+		--cidr-block 172.32.2.0/24 | jq '.Subnet.SubnetId' | tr -d '"'`
+echo "Created new private subnet with id $subnet_id"
 
 # Create an Internet Gateway
 ig_id=`aws ec2 create-internet-gateway \
-	--tag-specification '[{"ResourceType": "internet-gateway", "Tags": [{"Key": "Name", "Value": "IG-1"}]}]' | jq '.InternetGateway.InternetGatewayId'`
+	--tag-specification '[{"ResourceType": "internet-gateway", "Tags": [{"Key": "Name", "Value": "IG-1"}]}]' | jq '.InternetGateway.InternetGatewayId' | tr -d '"'`
 # Attach Internet Gateway to VPC
 aws ec2 attach-internet-gateway \
 	--internet-gateway-id $ig_id \
@@ -29,7 +34,7 @@ aws ec2 attach-internet-gateway \
 
 # Create a custom route table inside vpc but no assosciation with the subnets
 route_table_id=`aws ec2 create-route-table \
-		--vpc-id $vpc_id | jq '.RouteTable.RouteTableId'`
+		--vpc-id $vpc_id | jq '.RouteTable.RouteTableId' | tr -d '"'`
 echo "Created route table"
 # Create a route for the route table
 aws ec2 create-route \
@@ -47,6 +52,7 @@ echo "Assosciated the route table to public subnet"
 SSH_KEY_NAME="MyKeyPair"
 aws ec2 create-key-pair \
 	--key-name $SSH_KEY_NAME > mykey.pem
+# Update the SSH key to only contain content
 chmod 400 mykey.pem
 echo "Create ssh key"
 
@@ -56,7 +62,7 @@ SG_GROUP_NAME="my-security-group"
 sg_id=`aws ec2 create-security-group \
 	--group-name $SG_GROUP_NAME \
 	--vpc-id $vpc_id \
-	--description "Created Persnal SG" | jq '.GroupId'`
+	--description "Created Persnal SG" | jq '.GroupId' | tr -d '"'`
 echo "Created security group $SG_GROUP_NAME"
 # Add ssh access of inbound rule
 aws ec2 authorize-security-group-ingress \
@@ -75,5 +81,37 @@ instance_id=`aws ec2 run-instances \
 	--security-group-ids $sg_id \
 	--subnet-id $subnet_id \
 	--associate-public-ip-address \
-	--block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":20,"VolumeType":"gp2"}}]' | jq '.Instances[0].InstanceId'`
+	--block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":20,"VolumeType":"gp2"}}]' | jq '.Instances[0].InstanceId' | tr -d '"'`
 echo "Created instance $instance_id"
+
+# Create an elastic IP
+elastic_ip=`aws ec2 allocate-address\
+	--domain vpc | jq '.AllocationId' | tr -d '"'`
+# Create a Nat gateway and associate the IP
+ng_id=`aws ec2 create-nat-gateway \
+	--subnet-id $subnet_id \
+	--allocation-id $elastic_ip | jq '.NatGateway.NatGatewayId' | tr -d '"'`
+# Create a route table
+route_table_id_pvt=`aws ec2 create-route-table \
+		--vpc-id $vpc_id | jq '.RouteTable.RouteTableId' | tr -d '"'`
+# Create a route for the route table
+aws ec2 create-route \
+	--route-table-id $route_table_id_pvt \
+	--destination-cidr-block 0.0.0.0/0 \
+	--nat-gateway-id $ng_id
+echo "Created routes for route table"
+# Assosciate the route table to the public subnet
+aws ec2 associate-route-table \
+	--route-table-id $route_table_id_pvt \
+	--subnet-id  $subnet_id_pvt
+echo "Assosciated the route table to public subnet"
+
+# Create a ec2 instance in private subnet and it will be able to access internet now
+instance_id_pvt=`aws ec2 run-instances \
+		--image-id ammi-0715c1897453cabd1 \
+		--count 1 --instance-type t2.micro --key-name MyKeyPair \
+		--security-group-id $sg_id \
+		--subnet-id $subnet_id_pvt \
+		--block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":20,"VolumeType":"gp2"}}]' | jq '.Instances[0].InstanceId' | tr -d '"'`
+echo "Created instance in pvt subnet"
+
